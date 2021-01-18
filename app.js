16efbe99d5e722a10716f1c29fe7583c32c94adc
Michael Squires <michaelsquires@hotmail.co.uk>
2021-01-18 11:35:46 +0000

    adds Cors to app

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const apiRouter = require('./routers/api-router');
 const {
     send404,
-    send405,
     handleSQLErrors,
     handleCustomErrors,
     handleInternalErrors
 } = require('./controllers/errors');
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', apiRouter);
 app.all('/*', send404)
 
 app.use(handleSQLErrors);
-app.use(handleCustomErrors)
-app.use(handleInternalErrors)
+app.use(handleCustomErrors);
+app.use(handleInternalErrors;)
 
 module.exports = app
